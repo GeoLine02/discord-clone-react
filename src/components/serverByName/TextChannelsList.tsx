ee1e8d596790bf64432df983dfb3b11db641ac4a
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-10-31 17:21:16 +0400

    feat: channel creation

--- src/components/serverByName/TextChannelsList.tsx
+++ src/components/serverByName/TextChannelsList.tsx
@@ -1,28 +1,33 @@
 import { FaPlus } from "react-icons/fa";
 import TextChannel from "./TextChannel";
-import { IServer } from "../../types/servers";
+import { IChannel } from "../../types/channels";
 
 interface ITextChannelsListProps {
-  serverByName: IServer;
+  handleToogleChannelCreateModal: () => void;
+  channels: IChannel[];
 }
 
-const TextChannelsList = ({ serverByName }: ITextChannelsListProps) => {
+const TextChannelsList = ({
+  handleToogleChannelCreateModal,
+  channels,
+}: ITextChannelsListProps) => {
   return (
     <div>
       <div className="flex w-full items-center justify-between cursor-pointer">
         <h1 className="uppercase">text channels</h1>
-        <FaPlus />
+        <FaPlus onClick={handleToogleChannelCreateModal} />
       </div>
-      {serverByName?.channels.map(
-        (channel) =>
-          channel.channelType === "text" && (
-            <TextChannel
-              channelName={channel.channelName}
-              serverId={channel.serverId}
-              key={channel.id}
-            />
-          )
-      )}
+      {channels &&
+        channels.map(
+          (channel) =>
+            channel.channelType === "text" && (
+              <TextChannel
+                channelName={channel.channelName}
+                serverId={channel.serverId}
+                key={channel.id}
+              />
+            )
+        )}
     </div>
   );
 };
--- src/components/serverByName/VoiceChannelsList.tsx
+++ src/components/serverByName/VoiceChannelsList.tsx
@@ -1,28 +1,33 @@
 import { FaPlus } from "react-icons/fa";
 import VoiceChannel from "./VoiceChannel";
-import { IServer } from "../../types/servers";
+import { IChannel } from "../../types/channels";
 
 interface IVoiceChannelsListProps {
-  serverByName: IServer;
+  handleToogleChannelCreateModal: () => void;
+  channels: IChannel[];
 }
 
-const VoiceChannelsList = ({ serverByName }: IVoiceChannelsListProps) => {
+const VoiceChannelsList = ({
+  handleToogleChannelCreateModal,
+  channels,
+}: IVoiceChannelsListProps) => {
   return (
     <div>
       <div className="flex items-center justify-between w-full cursor-pointer">
         <h1 className="uppercase">voice channels</h1>
-        <FaPlus />
+        <FaPlus onClick={handleToogleChannelCreateModal} />
       </div>
-      {serverByName?.channels.map(
-        (channel) =>
-          channel.channelType === "voice" && (
-            <VoiceChannel
-              channelName={channel.channelName}
-              serverId={channel.serverId}
-              key={channel.id}
-            />
-          )
-      )}
+      {channels &&
+        channels.map(
+          (channel) =>
+            channel.channelType === "voice" && (
+              <VoiceChannel
+                channelName={channel.channelName}
+                serverId={channel.serverId}
+                key={channel.id}
+              />
+            )
+        )}
     </div>
   );
 };
